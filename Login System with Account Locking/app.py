32bd89919e66de5f0249cf2ff9938ad440f19ab2
Anoop Krishna <141720623+Anoop-2752@users.noreply.github.com>
2025-03-07 13:27:16 +0530

    Update app.py

--- Login System with Account Locking/app.py
+++ Login System with Account Locking/app.py
@@ -1,8 +1,4 @@
-'''
-Problem Statement
-Design a login system where a user can enter their username and password. The system should allow only three attempts for entering the correct credentials. If the user enters incorrect credentials three times, the account should be locked
 
-'''
 
 # Predefined user credentials
 users = {"john":"password123", "elliot":"mrrobot"}
